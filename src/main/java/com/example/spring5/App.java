22c92d2b3804f7d18fed7304669ddad985b66da5
diman <gaydukov89@gmail.com>
2020-04-27 03:00:41 +0800

    add test example

--- src/main/java/com/example/spring5/App.java
+++ src/main/java/com/example/spring5/App.java
@@ -1,6 +1,10 @@
 package com.example.spring5;
 
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App{
     public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(App.class.getPackageName());
     }
 }
--- src/test/java/com/example/spring5/MySimpleTest.java
+++ src/test/java/com/example/spring5/MySimpleTest.java
@@ -1,4 +1,32 @@
 package com.example.spring5;
 
 
+import com.example.logic.ann.props.Person1;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+//@ContextConfiguration
+//@TestPropertySource("classpath:application.yml")
+public class MySimpleTest extends AbstractJUnit4SpringContextTests {
+//    @Configuration
+//    @ComponentScan("com.example.logic.ann.props")
+//    static class Config{}
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void test(){
+        System.out.println(context);
+        System.out.println(applicationContext);
+    }
+}
